c4352d5782ee47f606876a17eafe190c991c8aba
Brouce Song <songwenwen@outlook.com>
2020-03-28 10:16:58 +0800

    update drone yml

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	//"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Printf("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world!"))
+	})
+
+	http.ListenAndServe("127.0.0.1:8080", nil)
 }
